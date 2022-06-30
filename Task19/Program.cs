67a946ee6cab6f87785336ecf512b89df900d7d9
Streltsov_Mike <streltsovmike@yandex.ru>
2022-06-30 12:19:02 +0300

    Add 19 task

---
+++ Task19/Program.cs
@@ -0,0 +1,43 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+// ПРОГРАММА ОПРЕДЕЛЯЕТ ПОЛИНДРОМ НЕЗАВИСИМО ОТ КОЛ-ВА ЦИФР В ЧИСЛЕ
+// ЕСЛИ НЕОБХОДИМА ПРОВЕРКА НА ПЯТИЗНАЧНОСТЬ, СОТРИТЕ ВСЕ НИЖЕСТОЯЩИЕ "//"
+
+
+int Revers(int arg)
+{
+    int revers = 0, n;
+    while (arg > 0)
+    {
+        n = arg % 10;
+        revers = revers * 10 + n;
+        arg = arg / 10;
+    }
+    return revers;
+}
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//if (number < 99999 && number > 10000)
+//{
+
+    int revers = Revers(number);
+
+
+    if (number == revers)
+    {
+        Console.WriteLine("Это полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Это не полиндром");
+    }
+//}
+//else
+//{
+//    Console.WriteLine("Вы ввели не пятизначное число");
+//}
